f265703bb39924ea994830f85e9987138d7c716b
Sridhar Mane <sridhars4s@gmail.com>
2016-01-20 21:13:43 -0500

    Init Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name resourceManagementApp
+ * @description
+ * # resourceManagementApp
+ *
+ * Main module of the application.
+ */
+angular.module('resourceManagementApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resourceManagementApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resourceManagementApp
+ */
+angular.module('resourceManagementApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
